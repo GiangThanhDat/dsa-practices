5b562954b5f0766405f2b57e72176d7c10e0a0a4
Giang Thanh Dat <gthanhdatpro@gmail.com>
2025-01-09 22:51:25 +0700

    solved: remove duplicates from sorted list II

---
+++ linked-list/medium/remove-duplicates-from-sorted-list-II.js
@@ -0,0 +1,96 @@
+console.log("Remove duplicates from sorted list II");
+
+/**
+ *
+ * Given the head of a sorted linked list,
+ * delete all nodes that have duplicate numbers,
+ * leaving only distinct numbers from the original list.
+ * Return the linked list sorted as well.
+ * Example 1 :
+ * Input: head = [1,2,3,3,4,4,5]
+ * Output: [1,2,5]
+ * Example 2:
+ * Input: head = [1,1,1,2,3]
+ * Output: [2,3]
+ */
+
+class ListNode {
+  constructor(val, next) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+// My first solution
+function deleteDuplicates(head) {
+  const dummy = new ListNode(0, head);
+
+  let prev = dummy,
+    curr = head;
+
+  while (prev.next && curr.next) {
+    let next = curr.next;
+    let hasDuplicate = false;
+
+    while (next && curr.val === next.val) {
+      curr.next = next.next;
+      next = next.next;
+      hasDuplicate = true;
+    }
+
+    if (hasDuplicate) {
+      prev.next = next;
+      curr = prev.next;
+    } else {
+      prev = prev.next;
+      curr = curr.next;
+    }
+  }
+
+  return dummy.next;
+}
+
+const testcases = [
+  new ListNode(
+    1,
+    new ListNode(
+      2,
+      new ListNode(
+        3,
+        new ListNode(3, new ListNode(4, new ListNode(4, new ListNode(5)))),
+      ),
+    ),
+  ),
+  new ListNode(
+    1,
+    new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3)))),
+  ),
+  new ListNode(
+    1,
+    new ListNode(
+      1,
+      new ListNode(
+        2,
+        new ListNode(2, new ListNode(2, new ListNode(3, new ListNode(4)))),
+      ),
+    ),
+  ),
+  new ListNode(1, new ListNode(1)),
+];
+
+function print(head) {
+  let out = "";
+  while (head) {
+    out += head.val;
+    head = head.next;
+  }
+  console.log(out);
+}
+
+for (const testcase of testcases) {
+  console.log("Input:");
+  print(testcase);
+  const output = deleteDuplicates(testcase);
+  console.log("Output:");
+  print(output);
+}
