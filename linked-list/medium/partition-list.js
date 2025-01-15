8068341f91471cb9f1cac9eb4a63d5440c2a840e
Giang Thanh Dat <gthanhdatpro@gmail.com>
2025-01-15 22:49:07 +0700

    fix(wip): try to solve partition problem

--- linked-list/medium/partition-list.js
+++ linked-list/medium/partition-list.js
@@ -23,13 +23,51 @@ console.log("Partition List");
 //   }
 // }
 
+// x = 3
+// Xac dinh thang cuoi cung < 3
+// va dau tien >= 3
+// loop lan nua xem thang nao < 3 thi dua ra truoc thang
+// dau tien >= 3
 function partition(head, x) {
+  let lastLast = null;
+  let firstGreaterOrEqal = null;
+
+  let prev = head;
+  let curr = head.next;
+
+  while (curr && lastLast === null && firstGreaterOrEqal === null) {
+    if (curr.val >= x) {
+      firstGreaterOrEqal = curr;
+      lastLast = prev;
+    } else {
+      prev = prev.next;
+      curr = curr.next;
+    }
+  }
+
+  prev = firstGreaterOrEqal;
+  curr = firstGreaterOrEqal.next;
+  while (curr) {
+    if (curr.val < x) {
+      const next = curr.next;
+      lastLast.next = curr;
+      curr.next = firstGreaterOrEqal;
+      prev.next = next;
+    }
+    console.log(prev.val, curr.val);
+    prev = prev.next;
+    curr = curr.next;
+  }
+
   return head;
 }
 
 const testcases = [
   [createLinkedList([1, 4, 3, 2, 5, 2]), 3],
   [createLinkedList([2, 1]), 2],
+  [createLinkedList([1, 7, 2, 1, 4, 5, 3, 2, 5, 2]), 3],
+  [createLinkedList([1, 2, 1, 3, 7, 2, 1, 4, 5, 3, 2, 5, 2]), 4],
+  // 1, 2, 1, 2, 2, 7, 4, 5, 3, 5
 ];
 
 for (const testcase of testcases) {
