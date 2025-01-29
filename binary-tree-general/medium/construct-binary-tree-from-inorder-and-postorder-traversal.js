ee4ccd61936e93b58793a23a1f1ca01a2dbb9850
Giang Thanh Dat <gthanhdatpro@gmail.com>
2025-01-29 22:26:07 +0700

    solved: construct binary tree from inorder and postorder traversal

--- binary-tree-general/medium/construct-binary-tree-from-inorder-and-postorder-traversal.js
+++ binary-tree-general/medium/construct-binary-tree-from-inorder-and-postorder-traversal.js
@@ -11,7 +11,66 @@ import { traversalPreorder } from "../utils.js";
  * */
 
 function buildTree(inorder, postorder) {
-  return null;
+  const inorderMap = new Map();
+
+  for (let i = 0; i < inorder.length; i++) {
+    inorderMap.set(inorder[i], i);
+  }
+
+  function createTree(
+    leftInorder,
+    rightInorder,
+    leftPostorder,
+    rightPostorder,
+  ) {
+    if (leftInorder > rightInorder) {
+      return null;
+    }
+    const val = postorder[rightPostorder];
+    const inorderIndex = inorderMap.get(val);
+    const leftSubtreeSize = inorderIndex - leftInorder;
+
+    return new TreeNode(
+      val,
+      createTree(
+        leftInorder,
+        inorderIndex - 1,
+        leftPostorder,
+        leftPostorder + leftSubtreeSize - 1,
+      ),
+      createTree(
+        inorderIndex + 1,
+        rightInorder,
+        leftPostorder + leftSubtreeSize,
+        rightPostorder - 1,
+      ),
+    );
+  }
+
+  return createTree(0, inorder.length - 1, 0, postorder.length - 1);
+}
+
+function buildTreeRightFirst(inorder, postorder) {
+  const inorderMap = new Map();
+
+  for (let i = 0; i < inorder.length; i++) {
+    inorderMap.set(inorder[i], i);
+  }
+
+  let postorderIndex = postorder.length - 1;
+  function createTree(left, right) {
+    if (left > right) return null;
+
+    const val = postorder[postorderIndex];
+    const inorderIndex = inorderMap.get(val);
+    postorderIndex--;
+    const root = new TreeNode(val);
+    root.right = createTree(inorderIndex + 1, right);
+    root.left = createTree(left, inorderIndex - 1);
+    return root;
+  }
+
+  return createTree(0, inorder.length - 1);
 }
 
 const testcases = [
@@ -20,20 +79,12 @@ const testcases = [
     [9, 15, 7, 20, 3],
   ],
   [
-    [9, 3, 15, 20, 7],
-    [9, 15, 7, 20, 3],
-  ],
-  [
-    [1, 2, 3, 5, 6, 8, 9],
-    [1, 3, 2, 6, 9, 8, 5],
+    [4, 2, 5, 1, 3],
+    [4, 5, 2, 3, 1],
   ],
   [
     [2, 5, 7, 10, 15, 20],
     [2, 7, 5, 20, 15, 10],
-  ],
-  [
-    [1, 2, 3],
-    [3, 2, 1],
   ],
   [[-1], [-1]],
 ];
@@ -41,6 +92,8 @@ const testcases = [
 for (const testcase of testcases) {
   const [inorder, postorder] = testcase;
   console.log("inorder:", inorder, "postorder:", postorder);
-  const tree = buildTree(inorder, inorder);
+  const tree = buildTree(inorder, postorder);
+  const tree2 = buildTreeRightFirst(inorder, postorder);
   traversalPreorder(tree);
+  traversalPreorder(tree2);
 }
