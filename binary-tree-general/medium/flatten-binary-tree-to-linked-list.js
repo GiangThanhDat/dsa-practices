9cb47b0229227ff97f9d44ca18f629ee9fced09a
Giang Thanh Dat <gthanhdatpro@gmail.com>
2025-02-16 16:19:56 +0700

    solved(binary-tree-general): solved problem without recursive

--- binary-tree-general/medium/flatten-binary-tree-to-linked-list.js
+++ binary-tree-general/medium/flatten-binary-tree-to-linked-list.js
@@ -11,8 +11,56 @@
  *   Output = [1,null,2,null,3,null,4, null,5,null,6]
  */
 
+import TreeNode from "../classes/tree-node.js";
+import { traversalPreorder } from "../utils.js";
+
 /**
  * Do not return anything, modify root in-place instaed.
  * */
 
-function flatten(root) {}
+// morris traversal
+function flatten(root) {
+  let curr = root;
+  while (curr) {
+    if (curr.left) {
+      let mostRightNode = curr.left;
+      while (mostRightNode.right) {
+        mostRightNode = mostRightNode.right;
+      }
+      mostRightNode.right = curr.right;
+      curr.right = curr.left;
+      curr.left = null;
+    }
+    curr = curr.right;
+  }
+}
+
+// stack
+function flattenWithStack(root) {
+  if (!root) return;
+  const stack = [root];
+  if (stack.length) {
+    const node = stack.pop();
+    if (node.right) stack.push(node.right);
+    if (node.left) stack.push(node.left);
+    if (stack.length) {
+      node.right = stack[stack.length - 1];
+    }
+    node.left = null;
+  }
+}
+
+const testcases = [
+  new TreeNode(
+    1,
+    new TreeNode(2, new TreeNode(3), new TreeNode(4)),
+    new TreeNode(5, null, new TreeNode(6)),
+  ),
+];
+
+for (const testcase of testcases) {
+  traversalPreorder(testcase);
+  flatten(testcase);
+  flattenWithStack(testcase);
+  traversalPreorder(testcase);
+}
