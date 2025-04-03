3f512ee368cbe9c56dac45d6f56b0db9402cebfc
Giang Thanh Dat <gthanhdatpro@gmail.com>
2025-04-03 22:02:17 +0700

    solved: number of islands problem

--- graph/medium/number-of-islands.js
+++ graph/medium/number-of-islands.js
@@ -18,53 +18,60 @@
  * Output = 1
  */
 
+// Giai phap Bfs tho so nhat
 function numIslands(grid) {
   const m = grid.length,
     n = grid[0].length;
-
-  const visited = new Array(m).fill(new Array(n).fill(false));
+  const visited = Array.from({ length: m }, () => Array(n).fill(false));
 
   function isValid(i, j) {
     return (
-      !(i < 0 || j < 0 || i > m || i > n) &&
+      i >= 0 &&
+      j >= 0 &&
+      i < m &&
+      j < n &&
       grid[i][j] === "1" &&
-      visited[(i, j)] === false
+      visited[i][j] === false
     );
   }
 
-  function bfs(i, j) {
-    const queue = [[i, j]];
+  const directions = [
+    [-1, 0], // tren
+    [1, 0], // duoi
+    [0, -1], // trai
+    [0, 1], // phai
+  ];
+
+  function bfs(row, col) {
+    const queue = [[row, col]];
+    visited[row][col] = true;
     let start = 0;
-    while (queue.length) {
-      const size = queue.length;
-      for (let left = start; left < size; left++) {
+    while (start < queue.length) {
+      const end = queue.length;
+      for (let left = start; left < end; left++) {
         const [i, j] = queue[left];
-        if (!visited[(i, j)]) {
-          visited[(i, j)] = true;
-        }
-        if (isValid(i - 1, j - 1)) {
-          queue.push([i - 1, j - 1]);
-        }
-        if (isValid(i - 1, j)) {
-          queue.push([i - 1, j]);
-        }
-        if (isValid(i - 1, j + 1)) {
-          queue.push([i - 1, j + 1]);
-        }
-        if (isValid(i, j - 1)) {
-          queue.push([i, j - 1]);
-        }
-        if (isValid(i, j + 1)) {
-          queue.push([i, j + 1]);
-        }
-        if (isValid(i + 1, j - 1)) {
-          queue.push([i + 1, j - 1]);
-        }
-        if (isValid(i + 1, j)) {
-          queue.push([i + 1, j]);
-        }
-        if (isValid(i + 1, j + 1)) {
-          queue.push([i + 1, j + 1]);
+        // if (isValid(i - 1, j)) {
+        //   queue.push([i - 1, j]);
+        //   visited[i - 1][j] = true;
+        // }
+        // if (isValid(i, j - 1)) {
+        //   queue.push([i, j - 1]);
+        //   visited[i][j - 1] = true;
+        // }
+        // if (isValid(i, j + 1)) {
+        //   queue.push([i, j + 1]);
+        //   visited[i][j + 1] = true;
+        // }
+        // if (isValid(i + 1, j)) {
+        //   queue.push([i + 1, j]);
+        //   visited[i + 1][j] = true;
+        // }
+        for (const [di, dj] of directions) {
+          const [ni, nj] = [i + di, j + dj];
+          if (isValid(ni, nj)) {
+            visited[ni][nj] = true;
+            queue.push([ni, nj]);
+          }
         }
       }
       start = end;
@@ -75,21 +82,27 @@ function numIslands(grid) {
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
       if (grid[i][j] === "1" && visited[i][j] === false) {
-        bfs(i, j);
-        console.log(`visited at count ${count}: `, visited);
         count++;
+        bfs(i, j);
       }
     }
   }
 
-  console.log("count:", count);
+  return count;
 }
 
 const testcases = [
   [
-    ["1", "1", "1", "1", "0"][("1", "1", "0", "1", "0")][
-      ("1", "1", "0", "0", "0")
-    ][("1", "1", "0", "0", "0")],
+    ["1", "1", "1", "1", "0"],
+    ["1", "1", "0", "1", "0"],
+    ["1", "1", "0", "0", "0"],
+    ["1", "1", "0", "1", "1"],
+  ],
+  [
+    ["1", "1", "0", "0", "0"],
+    ["1", "1", "0", "0", "0"],
+    ["0", "0", "1", "0", "0"],
+    ["0", "0", "0", "1", "1"],
   ],
 ];
 
